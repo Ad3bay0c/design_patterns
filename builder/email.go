8ac568d16589db18636e3d871ad977e2a6213c3f
Ad3bay0c <aclintonadebayo@gmail.com>
2022-03-08 09:32:34 +0100

    implemented builder for email

--- builder/email.go
+++ builder/email.go
@@ -1,1 +1,55 @@
 package main
+
+import "strings"
+
+type email struct {
+	from, to, body, subject string
+}
+
+type EmailBuilder struct {
+	email email
+}
+
+func (e *EmailBuilder) From(from string) *EmailBuilder {
+	if !strings.Contains(from, "@") {
+		panic("invalid email address")
+	}
+	e.email.from = from
+	return e
+}
+func (e *EmailBuilder) To(to string) *EmailBuilder {
+	if !strings.Contains(to, "@") {
+		panic("invalid email address")
+	}
+	e.email.to = to
+	return e
+}
+func (e *EmailBuilder) Subject(subject string) *EmailBuilder {
+	e.email.subject = subject
+	return e
+}
+func (e *EmailBuilder) Body(body string) *EmailBuilder {
+	e.email.body = body
+	return e
+}
+
+type build func(*EmailBuilder)
+
+func SendEmail(action build) {
+	builder := &EmailBuilder{}
+	action(builder)
+	SendEmailImpl(&builder.email)
+}
+
+func SendEmailImpl(email *email) {
+
+}
+
+func main() {
+	SendEmail(func(b *EmailBuilder) {
+		b.From("a@gmail.com").
+			To("b@gmail.com").
+			Subject("Hello").
+			Body("Hello world")
+	})
+}
