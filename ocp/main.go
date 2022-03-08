47e007202c12b6b5fa1ba0edddc3fd484e3f5960
Ad3bay0c <aclintonadebayo@gmail.com>
2022-03-08 09:36:26 +0100

    implemented open and close principle

--- ocp/main.go
+++ ocp/main.go
@@ -1,1 +1,30 @@
 package ocp
+
+type Rectangle struct {
+	breadth int
+	length	int
+}
+
+type Square struct {
+	length int
+}
+
+type Shape interface {
+	Area() int
+}
+
+func (r *Rectangle) Area() int {
+	return r.breadth * r.length
+}
+
+func (s *Square) Area() int {
+	return s.length * s.length
+}
+
+func sumShapes(shapes []Shape) int {
+	var sum int
+	for _, shape := range shapes {
+		sum += shape.Area()
+	}
+	return sum
+}
