1bd1c719ae09d1ac481b19ac4d5307581dcb3792
Ad3bay0c <aclintonadebayo@gmail.com>
2022-03-08 09:38:39 +0100

    implements builder

--- builder/main.go
+++ builder/main.go
@@ -1,1 +1,26 @@
-package builder
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+const (
+	indentSize = 2
+)
+type HTMLElement struct {
+	name string
+	text string
+	elements []HTMLElement
+}
+
+func geeralExample() {
+	sb := strings.Builder{}
+	sb.WriteString("Hello")
+	st := []strings.Builder{}
+	st = append(st, sb)
+	fmt.Println(st, sb.Len(), sb.Cap())
+}
+
+func main() {
+	geeralExample()
+}
