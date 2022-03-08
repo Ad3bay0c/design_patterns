02748f2ed5b348353a68ec26683f04cf73c9e78b
Ad3bay0c <aclintonadebayo@gmail.com>
2022-03-08 09:32:52 +0100

    implemented Functional builder for email

--- builder/functional.go
+++ builder/functional.go
@@ -1,1 +1,26 @@
 package main
+
+type Person1 struct {
+	name, position string
+}
+
+type personMod func(*Person1)
+
+type PersonBuilder1 struct {
+	actions []personMod
+}
+
+func (b *PersonBuilder1) Called(name string) *PersonBuilder1 {
+	b.actions = append(b.actions, func(p *Person1) {
+		p.name = name
+	})
+	return b
+}
+
+func (b *PersonBuilder1) Build() *Person1 {
+	p := &Person1{}
+	for _, action := range b.actions {
+		action(p)
+	}
+	return p
+}
