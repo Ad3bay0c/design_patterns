b28b0a973c7d7d7be9db2f3fb5fe4d084e939361
Ad3bay0c <aclintonadebayo@gmail.com>
2022-03-09 00:38:46 +0100

    implemented deepcopy prototype

--- prototype/deepCopying.go
+++ prototype/deepCopying.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+)
 
 type Address struct {
 	StreetAddress, City, Country string
@@ -8,17 +12,30 @@ type Address struct {
 type Person struct {
 	Name    string
 	Address *Address
+	Friends []string
 }
 
+func (p *Person) DeepCopy() *Person {
+	b := bytes.Buffer{}
+	e := gob.NewEncoder(&b)
+	e.Encode(p)
+	fmt.Println(string(b.Bytes()))
+
+	d := gob.NewDecoder(&b)
+	result := Person{}
+	d.Decode(&result)
+	return &result
+}
 func main() {
-	john := Person{"John", &Address{"123 London Road", "London", "UK"}}
+	john := Person{"John", &Address{"123 London Road", "London", "UK"}, []string{}}
 
 	jane := john
-	//jane.Address = &Address{
-	//	john.Address.StreetAddress,
-	//	john.Address.City,
-	//	john.Address.Country,
-	//}
+	// Deep Copy
+	jane.Address = &Address{
+		john.Address.StreetAddress,
+		john.Address.City,
+		john.Address.Country,
+	}
 	jane.Name = "Jane"
 	jane.Address.City = "Paris"
 
