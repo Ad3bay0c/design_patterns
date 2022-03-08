8415e5ede0bc48452df63a3df850ecb80c828108
Ad3bay0c <aclintonadebayo@gmail.com>
2022-03-08 09:35:54 +0100

    single responsiblity implemented

--- srp/main.go
+++ srp/main.go
@@ -1,1 +1,24 @@
 package srp
+
+type Rectangle struct {
+	breadth int
+	length	int
+}
+
+type Square struct {
+	length int
+}
+
+func (r *Rectangle) Area() int {
+	return r.breadth * r.length
+}
+
+func (s *Square) Area() int {
+	return s.length * s.length
+}
+
+func sumShapes() int {
+	r := Rectangle{breadth: 10, length: 5}
+	s := Square{length: 5}
+	return r.Area() + s.Area()
+}
